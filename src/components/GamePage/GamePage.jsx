fe56e9958c9c00b8aaa6c06f30b30f83a06bb5a2
Dustin Fedie <dlfedie@gmail.com>
2019-12-04 16:47:24 -0600

    chaged it to a function component, going to mess around with hooks, perhaps..

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { connect } from 'react-redux';
 
 
+function GamePage() {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
 
-class GamePage extends Component {
-    render () {
-        return (
-            <div>
-                <p>Hullo from game</p>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h1>Game Page</h1>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    );
 }
+
+// class GamePage extends Component {
+//     render () {
+//         return (
+//             <div>
+//                 <p>Hullo from game</p>
+//             </div>
+//         )
+//     }
+// }
 
 export default GamePage;
